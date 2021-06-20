3f635e7635e9dee6fde152522baf366cfb9e9d89
BadAtBlockchain <cautious.cryptocurrency@gmail.com>
2021-06-20 00:54:07 -0700

    day 25 updates

--- contracts/SuperRareToken.sol
+++ contracts/SuperRareToken.sol
@@ -51,6 +51,8 @@ contract SuperRareToken {
         }
 
         // 'count' should still be our length of available tokens
+        require(count == (supply - totalMinted), "available count does not equal (supply-minted)");
+
         uint randHash = uint(keccak256(abi.encodePacked(block.timestamp, msg.sender)));
         uint256 selection = randHash % count;
 
@@ -63,5 +65,36 @@ contract SuperRareToken {
         emit MintData(supply, count, totalMinted, selection);
 
         return selection;
+    }
+
+    // exposed utility for users
+    function totalAvailable() public view returns (uint8) {
+        return supply - totalMinted;
+    }
+
+    function ownerOf(uint _tokenId) public view returns (address){
+        // we make sure it's less than supply, as our tokenID's start at 0 in this project
+        // to start from one, we must do "owners[selection+1] = msg.sender" in the mint function
+        require(_tokenId < supply, "token must be less than supply (n-1)");
+        require(owners[_tokenId] != address(0), "token is not owned by anyone yet");
+
+        return owners[_tokenId];
+    }
+
+    // note, this will probably look horrible
+    function balanceOf(address _address) public view returns (uint8) {
+        require(_address != address(0), "Cannot check balance of 0x0");
+        
+        uint8 total = 0;
+
+        for (uint8 i = 0; i < supply; i++) {
+            if (owners[i] != address(0)) {
+                if (owners[i] == _address) {
+                    total++;
+                }
+            }
+        }
+
+        return total;
     }
 }
